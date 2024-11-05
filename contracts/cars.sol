0d09fc39c647ca9dd416e2f014cf740fb5ec0217
selmagudmundsen <selma.gudmundsen@gmail.com>
2024-11-05 12:33:48 +0100

    mint car

--- contracts/cars.sol
+++ contracts/cars.sol
@@ -1,19 +1,68 @@
-pragma solidity >= 0.8.0 <0.8.27;
-
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract CarRental is ERC721, Ownable {
+contract CarLeasingNFT is ERC721("CarLeasingNFT", "CLNFT") {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdTracker;
 
     struct Car {
         string model;
-        string color; 
-        uint64 year_of_matriculation;
-        uint64 original_value;
+        string color;
+        uint16 year_of_matriculation;
+        uint256 original_value;
     }
 
-    constructor(address initialOwner) {
+    struct LeaseDetail {
+        uint256 org_value;
+        uint256 current_mileage;
+        uint8 driver_experience;
+        uint256 mileage_cap;
+        uint256 contract_duration;
+    }
 
+    struct Payment {
+        address lessee;
+        uint256 amount;
+    }
+
+    mapping(uint256 => Car) public cars;
+    mapping(uint256 => LeaseDetail) public leaseDetails;
+    mapping(uint256 => Payment[]) public payments;
+
+    function mintCar(
+        address to,
+        string memory model,
+        string memory color,
+        uint16 year_of_matriculation,
+        uint256 original_value,
+        uint256 current_mileage,
+        uint8 driver_experience,
+        uint256 mileage_cap,
+        uint256 contract_duration
+    ) public {
+        uint256 tokenId = _tokenIdTracker.current();
+
+        cars[tokenId] = Car({
+            model: model,
+            color: color,
+            year_of_matriculation: year_of_matriculation,
+            original_value: original_value
+        });
+
+        leaseDetails[tokenId] = LeaseDetail({
+            org_value: original_value,
+            current_mileage: current_mileage,
+            driver_experience: driver_experience,
+            mileage_cap: mileage_cap,
+            contract_duration: contract_duration
+        });
+
+        _safeMint(to, tokenId);
+        _tokenIdTracker.increment();
     }
 }
